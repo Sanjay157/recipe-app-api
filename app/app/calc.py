880617f9a54d5a99c75ab94295ce7bdc47b647ee
S <sanjay.s3@dxc.com>
2019-05-29 11:53:17 +0530

    Added custom user model.

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+def add(x, y):
+    """Add two number and returns the result."""
+    
+    return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers and returns the result."""
+    return y - x
+    
